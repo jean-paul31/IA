59cffe92431ba7cfb39d9e7a5f799790c0bcffbc
jean-paul31 <andrei.jpaul@gmail.com>
2024-05-15 21:45:06 +0200

    test 2

--- first_ai.py
+++ first_ai.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 #voici un test
+#voici un test 2
 x_entry = np.array(([3,1.5],[2,1],[4,1.5],[3,1],[3.5,0.5],[2,0.5],[5.5,1],[1,1],[4,1.5]),dtype=float)
 y = np.array(([1],[0],[1],[0],[1],[0],[1],[0]),dtype=float) # données de sortie 1 = Rouge / 0 = bleu
 
